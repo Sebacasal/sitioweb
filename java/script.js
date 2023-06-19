c0d039286ff57925b95947a2b5f3a969f94c9e8c
sebacasal <seba.casal84@gmail.com>
2023-06-19 15:22:46 -0300

    javacomit

---
+++ java/script.js
@@ -0,0 +1,14 @@
+//operaciones: dos datos - obtener un tercer dato
+
+//aritmeticas + - * /
+
+let precioRemera=1300
+let precioPantalon=5000
+
+console.log(precioPantalon+precioRemera)
+
+
+console.log("hola" + "Don Jose") //concatenacion: me devuelve "hola Don Jose"
+
+//de comparacion
+//logicas
---
+++ java/script2.js
@@ -0,0 +1,21 @@
+//Variables
+//Palabras var(dejo de usarse) - let(reemplaza a la variable "var") - const(informacion que quiera guardar, no me deja cambiar el contenido al ser una constante)
+
+//declarar - crear la variable
+//estructura -> var nombre =valor
+
+let nombreAlumno="Juan Perez";
+let numero=464654;
+
+//utilizar la variable
+console.log(nombreAlumno)
+numero
+
+//cambiar contenido de la variable
+
+nombreAlumno="Ana Gomez"
+
+console.log(nombreAlumno)
+
+let otroAlumno="Ana Lopez";
+console.log(otroAlumno)
